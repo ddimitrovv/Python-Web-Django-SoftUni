e093620d3c020ef5b30467c64ccd6f9592a4805e
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-01-26 11:45:01 +0200

    added new tasks

--- web-basics/djangoProject/djangoProject/departments/views.py
+++ web-basics/djangoProject/djangoProject/departments/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 from djangoProject.departments.models import Departments
@@ -8,15 +8,41 @@ def show_department(request):
     return HttpResponse('Department Details Page')
 
 
+# def show_department_by_id(request, department_id):
+#     department_name = ''
+#     if department_id == 1:
+#         department_name = 'Developers'
+#     elif department_id == 2:
+#         department_name = 'Trainers'
+#
+#     html = f'<html><body><h1> Department Name: {department_name},' \
+#            f' Department ID: {department_id} <h1><body><html>'
+#
+#     return HttpResponse(html)
+
+
+# def show_department_by_id(request, department_id):
+#     department = Departments.objects.get(id=department_id)
+#     context = {
+#         'department_name': department.name,
+#         'department_id': department.pk
+#     }
+#
+#     return render(
+#         request=request,
+#         template_name='tasks/department-details.html',
+#         context=context
+#     )
+
+
 def show_department_by_id(request, department_id):
-    # if department_id == 1:
-    #     department_name = 'Developers'
-    # elif department_id == 2:
-    #     department_name = 'Trainers'
-
-    department = Departments.objects.get(id=department_id)
-
-    html = (f'<html><body><h1> Department Name: {department.name},'
-            f' Department ID: {department.pk} <h1><body><html>')
-
-    return HttpResponse(html)
+    department = Departments.objects.filter(id=department_id)
+    if department:
+        department = department.get()
+        context = {
+            'department_name': department.name,
+            'department_id': department.pk
+        }
+        return render(request, 'tasks/department-details.html', context)
+    else:
+        raise Http404
