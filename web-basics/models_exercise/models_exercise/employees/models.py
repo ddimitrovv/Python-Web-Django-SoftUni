26ad0ec35de53c74c36cc6aee2dd853729c74492
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-02-03 11:39:30 +0200

    added models exercises

--- web-basics/models_exercise/models_exercise/employees/models.py
+++ web-basics/models_exercise/models_exercise/employees/models.py
@@ -4,12 +4,17 @@ from django.db import models
 class Employee(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    emai_address = models.EmailField()
-    works_full_time = models.BooleanField()
-    job_level = models.CharField(max_length=20)
-    photo = models.URLField()
-    birth_date = models.DateField()
+    emai_address = models.EmailField(unique=True)
+    works_full_time = models.BooleanField(default=True)
+    job_level = models.CharField(
+        max_length=20,
+        default='Junior'
+    )
+    photo = models.URLField(blank=True)
+    birth_date = models.DateField(
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return f'{self.first_name} {self.last_name} - {self.job_level}'
-    
