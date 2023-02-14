36c1c6262bfe7bcf1ab3092a4c2f1a3ded923543
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-02-14 12:40:06 +0200

    added views

--- web-basics/Python_Web_Basics_Exam_01_November_2020/Python_Web_Basics_Exam_01_November_2020/recipes/models.py
+++ web-basics/Python_Web_Basics_Exam_01_November_2020/Python_Web_Basics_Exam_01_November_2020/recipes/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from project_validators.custom_validators import comma_separated
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=30)
     image_url = models.URLField()
     description = models.TextField()
-    ingredients = models.CharField(max_length=250)
+    ingredients = models.CharField(max_length=250, validators=[comma_separated])
     time = models.IntegerField()
 
     def __str__(self):
