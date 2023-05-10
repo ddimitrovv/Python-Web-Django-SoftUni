cf12876c2f9dc786e6f53188e134d646087c07fe
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-05-10 12:06:33 +0300

    added forms

---
+++ web-basics/Games_Play_App/Games_Play_App/web/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from Games_Play_App.web.models import Profile
+
+
+class ProfileBaseForm(forms.ModelForm):
+    # password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Profile
+        widgets = {
+            'password': forms.PasswordInput,
+        }
