56f9c2489900efdd70a6f652290d503f82f03041
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-05-23 14:47:50 +0300

    added new task

---
+++ web-basics/My_Plant_App/My_Plant_App/web/urls.py
@@ -0,0 +1,15 @@
+urlpatterns = (
+
+)
+
+# TODO:
+# • http: // localhost: 8000 / - home page
+# • http: // localhost: 8000 / profile / create / - profile create page
+# • http: // localhost: 8000 / catalogue / - catalogue
+# • http: // localhost: 8000 / create / - plant create page
+# • http: // localhost: 8000 / details / < plant_id > / - plant details page
+# • http: // localhost: 8000 / edit / < plant_id > / - plant edit page
+# • http: // localhost: 8000 / delete / < plant_id > / - plant delete page
+# • http: // localhost: 8000 / profile / details / - profile details page
+# • http: // localhost: 8000 / profile / edit / - profile edit page
+# • http: // localhost: 8000 / profile / delete / - profile delete page
