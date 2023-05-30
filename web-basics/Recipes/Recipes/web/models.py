d27030f9159db148de4b59ab091017757b0f3b03
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-05-30 11:43:17 +0300

    added models

--- web-basics/Recipes/Recipes/web/models.py
+++ web-basics/Recipes/Recipes/web/models.py
@@ -1,3 +1,40 @@
+from django.core.validators import MaxLengthValidator
+from django.db import models
 
 
+class Recipe(models.Model):
+    TITLE_MAX_LEN = 30
+    INGREDIENTS_MAX_LEN = 250
 
+    title = models.CharField(
+        max_length=TITLE_MAX_LEN,
+        blank=False,
+        null=False,
+        verbose_name='Title',
+    )
+
+    image = models.URLField(
+        blank=False,
+        null=False,
+        verbose_name='Image URL',
+    )
+
+    description = models.CharField(
+        blank=False,
+        null=False,
+        verbose_name='Description',
+    )
+
+    ingredients = models.TextField(
+        max_length=INGREDIENTS_MAX_LEN,
+        blank=False,
+        null=False,
+        verbose_name='Ingredients',
+    )
+
+    time_to_prepare = models.PositiveIntegerField(
+        blank=False,
+        null=False,
+        verbose_name='Time',
+    )
+    
