f49476691ae17a2efca8c792dfc2b6c035a749ec
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-05-30 11:29:13 +0300

    added new task

---
+++ web-basics/Recipes/Recipes/web/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+urlpatterns = (
+
+)
+
+# TODO:
+# • '/' - home page
+# • '/create' - create recipe page
+# • '/edit/:id' - edit recipe page
+# • '/delete/:id' - delete recipe page
+# • '/details/:id' - recipe details page
